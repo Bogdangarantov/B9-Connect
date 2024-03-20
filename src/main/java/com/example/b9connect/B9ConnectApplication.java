2744fb8d29877d56c7d5e47077e03298c64a6c96
BogDan <harysh.bogdan@gmail.com>
2024-03-20 05:09:53 +0200

    initial commit

---
+++ src/main/java/com/example/b9connect/B9ConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.b9connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B9ConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B9ConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/b9connect/B9ConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.b9connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B9ConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
